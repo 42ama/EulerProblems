4d0878bd9051fb22f6c1fb4490adc085107f10fd
42ama <45532431+42ama@users.noreply.github.com>
2019-11-20 11:50:47 +0400

    Добавлен новый Helper для поиска делителей числа. Новые методы: -Divisors - возвращает ISet делителей числа -DivisorsCount - возвращает число делителей числа

---
+++ EulerProblems/Utility/Helpers/Divisors.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EulerProject.Utility.Helpers
+{
+    public static partial class Helper
+    {
+        public static int DivisorsCount(long number)
+        {
+            return Divisors(number).Count;
+        }
+
+        public static ISet<long> Divisors(long number)
+        {
+            // первый общий делитель - единица
+            HashSet<long> set = new HashSet<long> { 1 };
+            for (long i = 2; i <= (long)Math.Floor(Math.Sqrt(number)); i++)
+            {
+                if (number % i == 0)
+                {
+                    set.Add(i);
+                    if (number / i != i)
+                    {
+                        set.Add(number / i);
+                    }
+                }
+            }
+            // второй общий делитель - само число
+            set.Add(number);
+            return set;
+        }
+    }
+}
