67af9edbc5f06c84614361ba05c060fe1f0b53dd
42ama <45532431+42ama@users.noreply.github.com>
2019-11-25 13:30:14 +0400

    Добавлено решение проблемы 15.

--- EulerProblems/Program.cs
+++ EulerProblems/Program.cs
@@ -21,7 +21,7 @@ namespace EulerProblems.Main
             {
                 typeof(One), typeof(Two), typeof(Three), typeof(Four), typeof(Five), 
                 typeof(Six), typeof(Seven), typeof(Eight), typeof(Nine), typeof(Ten),
-                typeof(Eleven), typeof(Twelve), typeof(Thirteen), typeof(Fourteen),
+                typeof(Eleven), typeof(Twelve), typeof(Thirteen), typeof(Fourteen), typeof(Fiveteen)
             };
 
             
