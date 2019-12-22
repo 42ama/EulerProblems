43288b7c222cbe3556f056eb5486835d58e4d395
42ama <45532431+42ama@users.noreply.github.com>
2019-12-22 12:07:35 +0400

    Добавлен метод в Helper: -GetAllDigits - возвращает все цифры в числе.

---
+++ EulerProblems/Model/Utility/Helpers/GetAllDigits.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EulerProblems.Model.Utility.Helpers
+{
+    public static partial class Helper
+    {
+        /// <summary>
+        /// Возвращает список цифр из числа
+        /// </summary>
+        public static IEnumerable<int> GetAllDigits(int number)
+        {
+            var digits = new List<int>();
+
+            while(number > 0)
+            {
+                digits.Add(number % 10);
+                number /= 10;
+            }
+
+            digits.Reverse();
+
+            return digits;
+        }
+
+        /// <summary>
+        /// Возвращает список цифр из числа
+        /// </summary>
+        public static IEnumerable<int> GetAllDigits(long number)
+        {
+            var digits = new List<int>();
+
+            while (number > 0)
+            {
+                digits.Add((int)(number % 10));
+                number /= 10;
+            }
+
+            digits.Reverse();
+
+            return digits;
+        }
+    }
+}
