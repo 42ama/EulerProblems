8eb60fc6d934548670049a8c517fccae03de0cea
42ama <45532431+42ama@users.noreply.github.com>
2019-12-22 11:18:12 +0400

    Добавлен новый метод в Helper: -Greatest Common Divisor - находит наибольший общий делитель двух чисел

---
+++ EulerProblems/Model/Utility/Helpers/GreatestCommonDivisor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EulerProblems.Model.Utility.Helpers
+{
+    public static partial class Helper
+    {
+        public static int GreatestCommonDivisor(int a, int b)
+        {
+            int y;
+            int x;
+
+            if (a > b)
+            {
+                x = a;
+                y = b;
+            }
+            else
+            {
+                x = b;
+                y = a;
+            }
+
+            while (x % y != 0)
+            {
+                int temp = x;
+                x = y;
+                y = temp % x;
+            }
+
+            return y;
+        }
+    }
+}
