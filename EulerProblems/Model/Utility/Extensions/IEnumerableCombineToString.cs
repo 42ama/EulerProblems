827b52af184db6374b0b6fe1cee1c132ec038780
42ama <45532431+42ama@users.noreply.github.com>
2020-01-03 13:16:53 +0400

    Добавлен метод расширения для IEnumerable<T>: -CombineToString - собирает строку из элементов коллекции

---
+++ EulerProblems/Model/Utility/Extensions/IEnumerableCombineToString.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EulerProblems.Model.Utility.Extensions
+{
+    public static partial class Extension
+    {
+        /// <summary>
+        /// Собирает коллекцию в сторку используя ToString()
+        /// </summary>
+        /// <param name="collection">Коллекция</param>
+        /// <returns>Строка из элементов коллекции</returns>
+        public static string CombineToString<T>(this IEnumerable<T> collection)
+        {
+            var sb = new StringBuilder();
+            foreach (var item in collection)
+            {
+                sb.Append(item.ToString());
+            }
+            return sb.ToString();
+        }
+
+    }
+}
